d94f12e1ed742fcc3858bf91507eac618ce2eaf9
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2022-01-07 15:51:18 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Daniel Pawelko
+# Created on: Jan 2022
+# One to Two Thousand
+
+
+def main():
+    # main function for creating one to two program
+
+    # process
+    for num in range(1000, 2000, 5):
+        # output
+        print(f"{num} {num+1} {num+2} {num+3} {num+4}")
+    # output
+    print("2000")
+
+    # done
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
